2b0758d4d39d08215c18670a47164bc3cdecc4a4
erinmayg <erinmaygunawan@gmail.com>
2021-02-09 04:59:04 +0800

    Add Sign Up / Login

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/hero.css';
 import Video from '../resources/video.mp4';
+import SignUp from './SignUp';
 
 function Hero() {
+  const [signUp, setSignUp] = useState(false);
+
   return (
     <div className='hero'>
       <div className='hero-bg'>
@@ -16,19 +19,27 @@ function Hero() {
         />
       </div>
       <div className='hero-content'>
-        <h1>
-          <span className='green'>Virtual Banking</span> <br />
-          has never been easier
-        </h1>
-        <h2>
-          Transfer from
-          <br />
-          various banks to various banks
-          <br />
-          for free
-        </h2>
+        <div className={'hero-content-text ' + (signUp ? 'left' : '')}>
+          <h1>
+            <span className='green'>Virtual Banking</span> <br />
+            has never been easier
+          </h1>
+          <h2>
+            Transfer from
+            <br />
+            various banks to various banks
+            <br />
+            for free
+          </h2>
 
-        <h3 className='button'>Sign Up</h3>
+          <h3
+            className={signUp ? 'none' : 'button'}
+            onClick={() => setSignUp(true)}
+          >
+            Sign Up
+          </h3>
+        </div>
+        {signUp && <SignUp setSignUp={setSignUp} />}
       </div>
     </div>
   );
